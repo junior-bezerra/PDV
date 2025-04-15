bd6c2496151a7abed8bf9c1ffe3dd8ca341007db
Júnior Bezerra <antonioalvesbjunior@gmail.com>
2025-04-14 21:19:37 -0300

    Acrescentado teste no API com Swagger

--- src/main/java/com/pdv/sistemapdv/controller/AuthController.java
+++ src/main/java/com/pdv/sistemapdv/controller/AuthController.java
@@ -2,19 +2,26 @@ package com.pdv.sistemapdv.controller;
 
 import com.pdv.sistemapdv.model.Usuario;
 import com.pdv.sistemapdv.repository.UsuarioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Operation(summary = "Registrar novo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário registrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos")
+    })
     @PostMapping("/register")
     public String registrarUsuario(@RequestBody Usuario usuario) {
         usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
